13c8bd4b1f2dffe2cea42f4bf2c398aa278feb38
NourhanMaged <nourhan.maged.initium@gmail.com>
2022-02-24 16:13:39 +0200

    cards , header and footer blade

---
+++ resources/views/site/footer.blade.php
@@ -0,0 +1,31 @@
+<footer class="text-center" style="background-color:#0E67CB; width: 100%">
+<!-- Grid container -->
+<div class="container">
+  <div class="row">
+    <div class="col-md-3">
+		<div class="row">
+		<img src="img/MicrosoftTeams-image (12).png" width="220" height="220" alt=""/>
+		</div>
+	  </div>
+    <div class="col-md-6 pt-5">
+      <div class="row pt-5">
+        <div class="col-md-2" ><a href="" style="font: 20px/37px Cairo; color: #FFFFFF;">FAQ</a> </div>
+        <div class="col-md-4"><a href="" style="font: 20px/37px Cairo; color: #FFFFFF;">Privacy Policy</a></div>
+        <div class="col-md-6"><a href="" style="font: 20px/37px Cairo; color: #FFFFFF;">Terms and Conditions</a></div>
+      </div>
+	  </div>
+      <div class="col-md-3 pt-5">
+		  <div class="row pt-5 justify-content-center">
+        <div class="col-md-3 pt-2"><img src="img/Path 60.svg" alt=""/></div>
+        <div class="col-md-3 pt-2"><img src="img/Path 56.svg" alt=""/></div>
+        <div class="col-md-3 pt-2"><img src="img/insta.svg" alt=""/></div>
+      </div>
+		</div>
+    </div>
+  </div>
+  <!-- Grid container --> 
+  
+  <!-- Copyright -->
+  <div class="text-center pb-5" style="font: 14px/26px Cairo; color: #FFFFFF;">© Initium Solutions 2022 | All Rights Reserved</div>
+  <!-- Copyright -->
+  </footer>
---
+++ resources/views/site/header.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg fixed-top navbar-light">
+  <div class="row">
+    <div class="collapse navbar-collapse mr-5"></div>
+    <div class="collapse navbar-collapse ml-5 mr-5 pl-5 pr-5"></div>
+    <a class="navbar-brand pl-5 ml-5 pt-2 pb-3 pr-5" href="#"><img src="img/OneMeLogo.png" width="120" height="57" alt=""/></a>
+    <button style="" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item"> <a style="font: 20px/32px Cairo; color: #1F2933;" class="nav-link" href="#">Home<span class="sr-only">(current) </span></a> </li>
+        <li class="nav-item ml-3"> <a style="font: 20px/32px Cairo; color: #1F2933;" class="nav-link" href="#">How it Works</a> </li>
+        <li class="nav-item ml-3 pr-5"> <a style="font: 20px/32px Cairo; color: #1F2933;" class="nav-link" href="#">1Me Pro</a> </li>
+      </ul>
+		<div class="collapse navbar-collapse ml-5 mr-5 pl-5 pr-5"></div>
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item pr-3 pt-2"> <a style="font: 20px/32px Cairo;" class="btn btn-block pt-2" id="proBtn" href="#">Get 1Me PRO </a> </li>
+        <li class="nav-item dropdown"> <a style="color: #1F2933;" class="nav-link dropdown-toggle btn-lg" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img class="mr-3" src="img/Ellipse 46.png" width="60" height="60" alt=""/></a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown"><a style="font: normal normal normal 14px/26px Cairo; color: #52606D;" class="dropdown-item" href="#">Change Avatar</a> <a style="font: normal normal normal 14px/26px Cairo; color: #52606D;"class="dropdown-item" href="#">Change Password</a> </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
